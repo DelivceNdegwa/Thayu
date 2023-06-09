779970432205ab0e80fbb90e76ebec106726bdb7
DelivceNdegwa <delivce.ndegwa18@gmail.com>
2023-06-09 18:13:35 +0300

    Updated .gitignore

---
+++ security/important.py
@@ -0,0 +1,4 @@
+MAILTRAP_USER = '7cfcf916d80a0b'
+MAILTRAP_PASSWORD = '6397b428056b94'
+MAILTRAP_EMAIL = '2be766d523-b6911f@inbox.mailtrap.io'
+PROJECT_SECRET_KEY = 'django-insecure-+8s19m0-5l5jq6h90^2$v4u+ohs970d2owtxf18^d1q+@_+kfx'
